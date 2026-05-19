c8ae55d2cd6f9085b680c8c6fe655e4fb934ea39
code <31704341+johnyvino@users.noreply.github.com>
2026-05-19 00:34:17 -0700

    Add Faber-Castell 26 Piece Pitt Graphite Tin Set

--- items.js
+++ items.js
@@ -12,6 +12,7 @@ window.CURATED_DATA = {
         { id: 'vehicles',   label: 'vehicles' }
     ],
     items: [
+        { id: 'faber-castell-26-piece-pitt-graphite-tin-set', brand: 'Faber-Castell', name: '26 Piece Pitt Graphite Tin Set', category: 'tech', size: 'small', image: 'assets/items/faber-castell-26-piece-pitt-graphite-tin-set.png', link: 'https://amazon.com/dp/B00SYFIPJ6', popularity: 70, added_on: '2026-05-19' },
         { id: 'lamy-studio-fountain-pen-black', brand: 'LAMY studio', name: 'Fountain Pen black', category: 'stationery', size: 'large', image: 'assets/items/lamy-studio-fountain-pen-black.png', link: 'https://www.lamy.com/en-us/p/lamy-studio-fountain-pen/50723086500174', popularity: 70, added_on: '2026-05-19' },
         { id: 'peak-design-everyday-sling', brand: 'Peak Design', name: 'Everyday Sling', category: 'tech', size: 'small', image: 'assets/items/peak-design-everyday-sling.png', link: 'https://www.peakdesign.com/products/everyday-sling?Size=3L&Color=Black', popularity: 70, added_on: '2026-05-19' },
         { id: 'wandrd-prvke-v4-21l-backpack', brand: 'WANDRD', name: 'PRVKE V4 21L Backpack', category: 'carry', size: 'small', image: 'assets/items/wandrd-prvke-v4-21l-backpack.png', link: 'https://www.wandrd.com/products/prvke?variant=41232563634256', popularity: 70, added_on: '2026-05-19' },
